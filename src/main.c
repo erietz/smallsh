2427bf7412093d5d228193b454cdd1869bb433d9
Ethan Rietz <ewrietz@gmail.com>
2022-01-30 20:18:05 -0800

    fix struct that caused lots of problems

--- src/main.c
+++ src/main.c
@@ -2,48 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const int MAX_CHARS = 2048;
-static const int MAX_ARGS = 512;
-static const char PROMPT[] = ": ";
+#define MAX_CHARS 2048
+#define MAX_ARGS 512
+#define PROMPT ": "
 
 typedef struct Args {
     int size;
-    char *items[];
+    char *items[MAX_ARGS];
 } Args;
 
 
 void read_input(char* user_input) {
-    memset(user_input, '\0', sizeof(&user_input));
+    char *tmp;
+
+    memset(user_input, '\0', MAX_CHARS*sizeof(char));
     printf(PROMPT);
-    fgets(user_input, MAX_CHARS, stdin);
+    tmp = fgets(user_input, MAX_CHARS, stdin);    // TODO: cannot read passed \n char
+    if (tmp == NULL) {
+        puts("something bad happened");
+    }
 }
 
 Args* parse_input(char* user_input) {
-    char *ptr, *token, *current_arg;
+    char *token, *current_arg;
     Args *args = malloc(sizeof(Args));
     args->size = 0;
+    args->items[0] = "";
 
-    token = strtok_r(user_input, " ", &ptr);
-    current_arg = calloc(strlen(token) + 1, sizeof(char));
-    strcpy(current_arg, token);
+    token = strtok(user_input, " \n");
+    current_arg = strdup(token);
     args->items[args->size] = current_arg;
     args->size += 1;
 
     while (token != NULL) {
-        token = strtok_r(NULL, " ", &ptr);
+        token = strtok(NULL, " \n");
         if (token != NULL) {
-            current_arg = calloc(strlen(token) + 1, sizeof(char));
-            strcpy(current_arg, token);
+            current_arg = strdup(token);
             args->items[args->size] = current_arg;
             args->size += 1;
         }
     }
 
-    // trim off trailing new line from last item in args array
-    int len = strlen(current_arg);
-    if (current_arg[len - 1] == '\n') {
-        current_arg[len - 1] = '\0';
-    }
+    /* TODO: If using fgets this is unnessary since I can use " \n" in strtok */
+    /* // trim off trailing new line from last item in args array */
+    /* int len = strlen(current_arg); */
+    /* if (current_arg[len - 1] == '\n') { */
+    /*     current_arg[len - 1] = '\0'; */
+    /* } */
+
     return args;
 }
 
@@ -53,6 +59,14 @@ void print_args(Args* args) {
     }
 }
 
+void free_args(Args* args) {
+    for (int i = 0; i < args->size; i++) {
+        free(args->items[i]);
+    }
+    /* free(args->items); */
+    free(args);
+}
+
 int main(int argc, char *argv[]) {
     char user_input[MAX_CHARS];
     Args *args;
@@ -60,7 +74,9 @@ int main(int argc, char *argv[]) {
     while (1) {
         read_input(user_input);
         args = parse_input(user_input);
+        printf("size %i\n", args->size);
         print_args(args);
+        free_args(args);
     }
 
     return 0;
