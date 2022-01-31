e78e1181a42c6d4acc3710d3ca99ab07aae06b94
Ethan Rietz <ewrietz@gmail.com>
2022-01-31 07:30:04 -0800

    simpifly my life

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ void read_input(char* input_buffer) {
 
     memset(input_buffer, '\0', MAX_CHARS*sizeof(char));
     printf(PROMPT);
-    raw_input = fgets(input_buffer, MAX_CHARS, stdin);    // TODO: cannot read passed \n char
+    raw_input = fgets(input_buffer, MAX_CHARS, stdin);
 
     if (raw_input == NULL) {
         puts("Error with fgets or EOF");
@@ -26,30 +26,19 @@ void read_input(char* input_buffer) {
 
 void parse_input(char* input_buffer, Args* args) {
     char *token;
-    /* char *current_arg; */
     args->size = 0;
 
     token = strtok(input_buffer, " \n");
-    /* current_arg = strdup(token); */
     strcpy(args->items[args->size], token);
     args->size += 1;
 
     while (token != NULL) {
         token = strtok(NULL, " \n");
         if (token != NULL) {
-            /* current_arg = strdup(token); */
-            /* args->items[args->size] = current_arg; */
             strcpy(args->items[args->size], token);
             args->size += 1;
         }
     }
-
-    /* TODO: If using fgets this is unnessary since I can use " \n" in strtok */
-    /* // trim off trailing new line from last item in args array */
-    /* int len = strlen(current_arg); */
-    /* if (current_arg[len - 1] == '\n') { */
-    /*     current_arg[len - 1] = '\0'; */
-    /* } */
 
 }
 
@@ -59,25 +48,18 @@ void print_args(Args* args) {
     }
 }
 
-void free_args(Args* args) {
-    for (int i = 0; i < args->size; i++) {
-        free(args->items[i]);
-    }
-    /* free(args->items); */
-    /* free(args); */
-}
-
 int main(int argc, char *argv[]) {
     char input_buffer[MAX_CHARS];
-    Args args;
-    args.size = 0;
+    Args args = {
+        .size = 0,
+        .items = {}
+    };
 
     while (1) {
         read_input(input_buffer);
         parse_input(input_buffer, &args);
         printf("size %i\n", args.size);
         print_args(&args);
-        /* free_args(*args); */
     }
 
     return 0;
