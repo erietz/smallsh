ffcab2fe707dee2938caf039eb7b2013c44faa87
Ethan Rietz <ewrietz@gmail.com>
2022-02-07 22:26:49 -0600

    add pid to finished background process

--- src/execute.c
+++ src/execute.c
@@ -351,6 +351,7 @@ void cleanup_bg_processes(BgProcess* bg_processes) {
         int pid = waitpid(curr->pid, &status, WNOHANG);
 
         if (pid > 0) {
+            printf("background pid %d is done: ", curr->pid);
             print_status(status);
             fflush(stdout);
             remove_bg_node(bg_processes, curr->pid);
